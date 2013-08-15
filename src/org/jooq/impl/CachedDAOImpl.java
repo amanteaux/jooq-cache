36d91906a867547b2268315707d07d97e7df64e0
Aurélien Manteaux <amanteaux@gmail.com>
2013-08-15 22:12:11 +0200

    Cache DAO added

---
+++ src/org/jooq/impl/CachedDAOImpl.java
@@ -0,0 +1,17 @@
+package org.jooq.impl;
+
+import org.jooq.Configuration;
+import org.jooq.Table;
+import org.jooq.UpdatableRecord;
+
+public abstract class CachedDAOImpl<R extends UpdatableRecord<R>, P, T> extends ExtendedDAOImpl<R, P, T> {
+
+	public CachedDAOImpl(final Table<R> table, final Class<P> type) {
+		super(table, type);
+	}
+
+	protected CachedDAOImpl(final Table<R> table, final Class<P> type, final Configuration configuration) {
+		super(table, type, configuration);
+	}
+
+}
