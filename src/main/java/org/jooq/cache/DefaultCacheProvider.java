42ca8dbd8aedda4be1dddcb54c546d29b5bcd741
Aurélien Manteaux <amanteaux@cardiweb.com>
2014-02-10 13:50:00 +0100

    Guava is used for the query cache

--- src/main/java/org/jooq/cache/DefaultCacheProvider.java
+++ src/main/java/org/jooq/cache/DefaultCacheProvider.java
@@ -1,5 +1,10 @@
 package org.jooq.cache;
 
+import java.io.Serializable;
+import java.util.concurrent.TimeUnit;
+
+import com.google.common.cache.CacheBuilder;
+
 
 
 public final class DefaultCacheProvider implements CacheProvider {
@@ -16,8 +21,30 @@ public final class DefaultCacheProvider implements CacheProvider {
 
 	@Override
 	public final Cache fetchByQuery(String query) {
-		// TODO do that a bit better :)
-		return new DefaultCache();
+		return new Cache() {
+			
+			private final com.google.common.cache.Cache<String, Serializable> cache = CacheBuilder
+					.newBuilder()
+					.maximumSize(10000)
+					.expireAfterAccess(2, TimeUnit.HOURS)
+					.build();
+			
+			@Override
+			public Serializable put(String key, Serializable value) {
+				cache.put(key, value);
+				return value;
+			}
+			
+			@Override
+			public Serializable get(String key) {
+				return cache.getIfPresent(key);
+			}
+			
+			@Override
+			public void clear() {
+				cache.invalidateAll();
+			}
+		};
 	}
 
 }
