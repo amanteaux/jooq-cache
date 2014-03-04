e3c23637b909c9f94f3fc777bedd4b81225a3e50
Aurélien Manteaux <amanteaux@cardiweb.com>
2014-03-04 19:56:26 +0100

    TU for CacheManager

--- src/test/java/org/jooq/cache/CacheManagerTest.java
+++ src/test/java/org/jooq/cache/CacheManagerTest.java
@@ -12,55 +12,43 @@ import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.ImmutableSet;
 
 public class CacheManagerTest {
-	
-    // --------------------------------------------------------------------------------
-    // XXX: index(Cache tableIndexes, String tableReferenceName, String query) testing
-    // --------------------------------------------------------------------------------
-	
-	@Test
-	public void should_use_set_for_table_index() {
-		CacheManager cacheManager = mockCacheManager();
-		DefaultCache cache = new DefaultCache();
-		
-		cacheManager.index(cache, "table", "SELECT * FROM table");
-		
-		assertThat(cache.get("table")).isInstanceOf(Set.class);
-	}
-	
-	@SuppressWarnings("unchecked")
-	@Test
-	public void should_add_a_table_and_a_query_to_the_table_index() {
-		CacheManager cacheManager = mockCacheManager();
-		DefaultCache cache = new DefaultCache();
-		
-		cacheManager.index(cache, "table", "SELECT * FROM table");
-		
-		assertThat(cache.get("table")).isNotNull();
-		assertThat((Set<String>) cache.get("table")).isEqualTo(ImmutableSet.of("SELECT * FROM table"));
-	}
-	
-	@SuppressWarnings("unchecked")
-	@Test
-	public void should_add_a_query_to_the_table_index() {
-		CacheManager cacheManager = mockCacheManager();
-		DefaultCache cache = new DefaultCache();
-		
-		cacheManager.index(cache, "table", "SELECT * FROM table WHERE a = ?");
-		cacheManager.index(cache, "table", "SELECT * FROM table WHERE b = ?");
-		
-		assertThat((Set<String>) cache.get("table")).isEqualTo(ImmutableSet.of("SELECT * FROM table WHERE a = ?", "SELECT * FROM table WHERE b = ?"));
-	}
-	
-    // --------------------------------------------------------------------------------
-    // XXX: tableIndex() and fetchByQuery(String query) testing
+
+	// --------------------------------------------------------------------------------
+    // XXX: getCachedDataIfPresent(String query, List<Object> queryParameters) testing
     // --------------------------------------------------------------------------------
 	
 	@Test
-	public void should_create_a_cache_only_once() {
+	public void should_return_null_because_no_data_exists() {
 		CacheManager cacheManager = mockCacheManager();
-		assertThat(cacheManager.fetchByQuery("SELECT * FROM table")).isSameAs(cacheManager.fetchByQuery("SELECT * FROM table"));
-		assertThat(cacheManager.tableIndex()).isSameAs(cacheManager.tableIndex());
+		
+		assertThat(cacheManager.getCachedDataIfPresent("SELECT * FROM table", ImmutableList.of())).isNull();
 	}
+	
+	@Test
+	public void should_return_the_cached_data() {
+		CacheManager cacheManager = mockCacheManager();
+		CachedData mockCachedData = mockCachedData();
+		
+		cacheManager.cacheQueryResult(ImmutableSet.of("table"), "SELECT * FROM table", ImmutableList.of(), mockCachedData);
+		
+		assertThat(cacheManager.getCachedDataIfPresent("SELECT * FROM table", ImmutableList.of())).isEqualTo(mockCachedData);
+	}
+	
+    // --------------------------------------------------------------------------------
+    // XXX: cacheQueryResult(...) testing
+    // --------------------------------------------------------------------------------
+	
+	@Test
+	public void should_add_a_query_result_to_the_query_cache_and_index_it_in_the_table_index() {
+		CacheManager cacheManager = mockCacheManager();
+		CachedData mockCachedData = mockCachedData();
+		
+		cacheManager.cacheQueryResult(ImmutableSet.of("table"), "SELECT * FROM table", ImmutableList.of(), mockCachedData);
+		
+		assertThat(cacheManager.tableIndex().get("table")).isEqualTo(ImmutableSet.of("SELECT * FROM table"));
+		assertThat(cacheManager.fetchByQuery("SELECT * FROM table").get(cacheManager.joinParameters(ImmutableList.of()))).isSameAs(mockCachedData);
+	}
+	
 	
     // --------------------------------------------------------------------------------
     // XXX: clearByQuery(String query) testing
@@ -96,6 +84,85 @@ public class CacheManagerTest {
 		assertThat(cacheManager.getCachedDataIfPresent("SELECT * FROM table1", ImmutableList.of())).isNull();
 		assertThat(cacheManager.getCachedDataIfPresent("SELECT * FROM table2", ImmutableList.of())).isNotNull();
 		assertThat(cacheManager.getCachedDataIfPresent("SELECT * FROM table2 t2 JOIN table1 t1 ON t2.a=t1.a", ImmutableList.of())).isNull();
+	}
+	
+	// internal
+	
+    // --------------------------------------------------------------------------------
+    // XXX: joinParameters(List<Object> queryParameters) testing
+    // --------------------------------------------------------------------------------
+	
+	@Test
+	public void should_concatenate_strings() {
+		CacheManager cacheManager = mockCacheManager();
+		
+		assertThat(cacheManager.joinParameters(ImmutableList.<Object>of("a", "b"))).isEqualTo("[a, b]");
+		assertThat(cacheManager.joinParameters(ImmutableList.<Object>of())).isEqualTo("[]");
+	}
+	
+    // --------------------------------------------------------------------------------
+    // XXX: tableIndex() and fetchByQuery(String query) testing
+    // --------------------------------------------------------------------------------
+	
+	@Test
+	public void should_create_a_cache_only_once() {
+		CacheManager cacheManager = mockCacheManager();
+		assertThat(cacheManager.fetchByQuery("SELECT * FROM table")).isSameAs(cacheManager.fetchByQuery("SELECT * FROM table"));
+		assertThat(cacheManager.tableIndex()).isSameAs(cacheManager.tableIndex());
+	}
+
+    // --------------------------------------------------------------------------------
+    // XXX: index(Cache tableIndexes, Set<String> referencedTables, String query) testing
+    // --------------------------------------------------------------------------------
+	
+	@SuppressWarnings("unchecked")
+	@Test
+	public void should_add_a_query_to_multiple_table_index() {
+		CacheManager cacheManager = mockCacheManager();
+		DefaultCache cache = new DefaultCache();
+		
+		cacheManager.index(cache, ImmutableSet.of("table1", "table2"), "SELECT * FROM table WHERE a = ?");
+		
+		assertThat((Set<String>) cache.get("table1")).isEqualTo(ImmutableSet.of("SELECT * FROM table WHERE a = ?"));
+		assertThat((Set<String>) cache.get("table2")).isEqualTo(ImmutableSet.of("SELECT * FROM table WHERE a = ?"));
+	}
+	
+    // --------------------------------------------------------------------------------
+    // XXX: index(Cache tableIndexes, String tableReferenceName, String query) testing
+    // --------------------------------------------------------------------------------
+	
+	@Test
+	public void should_use_a_set_for_the_table_index() {
+		CacheManager cacheManager = mockCacheManager();
+		DefaultCache cache = new DefaultCache();
+		
+		cacheManager.index(cache, "table", "SELECT * FROM table");
+		
+		assertThat(cache.get("table")).isInstanceOf(Set.class);
+	}
+	
+	@SuppressWarnings("unchecked")
+	@Test
+	public void should_add_a_table_and_a_query_to_the_table_index() {
+		CacheManager cacheManager = mockCacheManager();
+		DefaultCache cache = new DefaultCache();
+		
+		cacheManager.index(cache, "table", "SELECT * FROM table");
+		
+		assertThat(cache.get("table")).isNotNull();
+		assertThat((Set<String>) cache.get("table")).isEqualTo(ImmutableSet.of("SELECT * FROM table"));
+	}
+	
+	@SuppressWarnings("unchecked")
+	@Test
+	public void should_add_a_query_to_the_table_index() {
+		CacheManager cacheManager = mockCacheManager();
+		DefaultCache cache = new DefaultCache();
+		
+		cacheManager.index(cache, "table", "SELECT * FROM table WHERE a = ?");
+		cacheManager.index(cache, "table", "SELECT * FROM table WHERE b = ?");
+		
+		assertThat((Set<String>) cache.get("table")).isEqualTo(ImmutableSet.of("SELECT * FROM table WHERE a = ?", "SELECT * FROM table WHERE b = ?"));
 	}
 	
 	// utils
